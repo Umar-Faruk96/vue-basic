7c5ab96051aa85b332ff873b974ccc2bff41f08a
Omor Faruk Sheikh Raihan <omorfarukjabbar@gmail.com>
2024-12-08 20:47:14 +0600

    add vue lifecycle hook like onmounted, onupdated with example

---
+++ src/practices/InvokeMethod.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+import useUtils from "../utils";
+
+let number = ref(0);
+
+const { increment, decrement } = useUtils(number);
+</script>
+
+<template>
+  <div>
+    <!-- <button @click="increment">Increment</button> -->
+    <button @click="number++">Increment</button>
+    <p>Count: {{ number }}</p>
+    <!-- <button @click="decrement">Decrement</button> -->
+    <button @click="number--">Decrement</button>
+  </div>
+</template>
+
+<style scoped></style>
