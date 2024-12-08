9c323b1e45ff9ff30074ed62cee4633cc48fe936
Omor Faruk Sheikh Raihan <omorfarukjabbar@gmail.com>
2024-12-08 11:28:50 +0600

    First commit with template syntax practice in place

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import TemplateSyntax from './practices/TemplateSyntax.vue'
+
+createApp(App).mount('#app')
+// createApp(TemplateSyntax).mount('#app')
---
+++ src/practices/TemplateSyntax.vue
@@ -0,0 +1,33 @@
+<script setup>
+let msg = "Welcome Omor Faruk Sheikh Raihan";
+let dynamicId = 2;
+let isButtonDisabled = true;
+
+const objectOfAttributes = {
+  id: "container",
+  class: "wrapper",
+  style: "color: red; font-size: 20px",
+};
+
+let show = false;
+
+const clickMe = () => {
+  console.log("Clicked");
+};
+</script>
+
+<template>
+  <div v-bind:id="dynamicId">{{ msg }}</div>
+  <div v-html="msg"></div>
+
+  <button type="button" :disabled="isButtonDisabled" @click="clickMe">Disabled or Not?</button>
+
+  <div v-bind="objectOfAttributes">Multiple Data Binding</div>
+
+  <p v-if="show">Now you can see me! (if)</p>
+  <p v-show="show">Now you can see me! (show)</p>
+
+  <button v-on:click="clickMe">Click Me</button>
+</template>
+
+<style scoped></style>
